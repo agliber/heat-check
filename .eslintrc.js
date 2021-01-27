c27a984d365caaf9b17439df92ca4bc1a8cff384
AG Liber <aliber@usc.edu>
2021-01-26 17:07:46 -0800

    feat: customize eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    '@react-native-community',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    semi: ['error', 'always'],
+    'block-spacing': ['error', 'always'],
+    'no-shadow': ['off'],
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 1,
+    curly: ['error', 'multi-line'],
+  },
 };
